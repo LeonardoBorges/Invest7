23ddc43869461cd79f874279d7a326b59759b55a
pucheranni <108954830+pucheranni@users.noreply.github.com>
2025-03-24 15:19:00 -0300

    Update RendaFixaDAO.java

--- src/main/java/com/invest7/dao/RendaFixaDAO.java
+++ src/main/java/com/invest7/dao/RendaFixaDAO.java
@@ -1,47 +1,35 @@
 package com.invest7.dao;
 
+import com.invest7.model.produtos.RendaFixa;
 import com.invest7.util.ConnectionFactory;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
-import com.invest7.model.produtos.RendaFixa;
+
 public class RendaFixaDAO {
-
     public List<RendaFixa> buscarRendaFixa() {
-        List<RendaFixa> RendaFixaAll = new ArrayList<>();
-        String sql = "SELECT nome_produto, " +
-                "rentabilidade_bruta, " +
-                "rentabilidade_liquida, " +
-                "risco_financeiro, " +
-                "investimento_minimo, " +
-                "imposto_renda_IR FROM renda_fixa";
-                
+        List<RendaFixa> produtos = new ArrayList<>();
+        String sql = "SELECT nome_produto, rentabilidade_bruta, risco_financeiro, "
+                   + "investimento_minimo, tipo_produto, is_taxable FROM renda_fixa";
+        
         try (Connection conn = ConnectionFactory.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(sql)) {
-
-            try (ResultSet rs = stmt.executeQuery()) {
-                while (rs.next()) {
-                    RendaFixa acao = new RendaFixa(
-                            rs.getString("nome_produto"),
-                            rs.getDouble("rentabilidade_bruta"),
-                            rs.getDouble("rentabilidade_liquida"),
-                            rs.getInt("risco_financeiro"),
-                            rs.getDouble("investimento_minimo"),
-                            rs.getDouble("imposto_renda_IR")
-                    );
-
-                    RendaFixaAll.add(acao);
-                }
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+            
+            while (rs.next()) {
+                RendaFixa produto = new RendaFixa(
+                    rs.getString("nome_produto"),
+                    rs.getDouble("rentabilidade_bruta"),
+                    rs.getInt("risco_financeiro"),
+                    rs.getDouble("investimento_minimo"),
+                    rs.getString("tipo_produto"),
+                    rs.getBoolean("is_taxable")
+                );
+                produtos.add(produto);
             }
         } catch (SQLException e) {
-            System.err.println("Erro ao buscar renda fixa: " + e.getMessage());
-            return null;
+            System.err.println("Erro ao buscar produtos: " + e.getMessage());
         }
-
-        return RendaFixaAll;
+        return produtos;
     }
 }
