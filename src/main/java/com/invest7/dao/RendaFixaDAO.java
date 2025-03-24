63a92358b38aaf548660704c15ddc59db51f78dc
pucheranni <108954830+pucheranni@users.noreply.github.com>
2025-03-24 15:52:07 -0300

    Update RendaFixaDAO.java

--- src/main/java/com/invest7/dao/RendaFixaDAO.java
+++ src/main/java/com/invest7/dao/RendaFixaDAO.java
@@ -7,11 +7,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class RendaFixaDAO {
-    public List<RendaFixa> buscarRendaFixa() {
+    public List<RendaFixa> buscarTodosProdutos() {
         List<RendaFixa> produtos = new ArrayList<>();
-        String sql = "SELECT nome_produto, rentabilidade_bruta, risco_financeiro, "
-                   + "investimento_minimo, tipo_produto, is_taxable FROM renda_fixa";
-        
+        String sql = "SELECT nome_produto, tipo_produto, rentabilidade_bruta, rentabilidade_liquida, "
+                   + "risco_financeiro, investimento_minimo, is_taxable, taxa_base, porcentagem_taxa "
+                   + "FROM renda_fixa";
+
         try (Connection conn = ConnectionFactory.getConnection();
              PreparedStatement stmt = conn.prepareStatement(sql);
              ResultSet rs = stmt.executeQuery()) {
@@ -19,11 +20,14 @@ public class RendaFixaDAO {
             while (rs.next()) {
                 RendaFixa produto = new RendaFixa(
                     rs.getString("nome_produto"),
+                    rs.getString("tipo_produto"),
                     rs.getDouble("rentabilidade_bruta"),
+                    rs.getDouble("rentabilidade_liquida"),
                     rs.getInt("risco_financeiro"),
                     rs.getDouble("investimento_minimo"),
-                    rs.getString("tipo_produto"),
-                    rs.getBoolean("is_taxable")
+                    rs.getBoolean("is_taxable"),
+                    rs.getString("taxa_base"),
+                    rs.getDouble("porcentagem_taxa")
                 );
                 produtos.add(produto);
             }
